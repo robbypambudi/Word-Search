191210a8b913543ecf73cbb5c2caf223b56c23e8
Fathin Muhashibi Putra <fathinmp11@gmail.com>
2023-05-15 08:21:08 +0700

    feat: update main.py

--- main.py
+++ main.py
@@ -1,38 +1,40 @@
 import string
-import tkinter as tk  # Merupakan modul untuk membuat GUI
-import tkinter.messagebox as msg  # Merupakan modul untuk membuat pesan pada GUI
-from tkinter import ttk  # Merupakan modul untuk membuat widget tambahan pada tkinter
+import tkinter as tk
+import tkinter.messagebox as msg
+from tkinter import ttk
 import src.gameUtils as gutils
 import src.Game as game
-root = tk.Tk()  # Membuat objek root sebagai GUI utama
+
+root = tk.Tk()
 root.title("Word Search Game")
 
 # Init
 
 
 def init():
-    header = tk.Frame(root)
+    # menambahkan width dan height untuk membuat frame persegi
+    header = tk.Frame(root, bg="white", width=400, height=100)
     header.pack(fill=tk.BOTH, side=tk.TOP)
 
-    # FOnt bold
-    heading = tk.Label(header, text="Search Me If You Can", font=(
-        "Open Sans", 16, "bold"), fg='black')  # Fg = foreground merupakan warna dari text
+    # Mengubah warna latar belakang menjadi putih
+    root.config(background='white')
 
-    # Expand = True agar heading dapat mengisi seluruh ruang yang tersedia pada frame
+    heading = tk.Label(header, text="Search Me If You Can", font=(
+        "Open Sans", 16, "bold"), fg='black', bg='white')
+
     heading.pack(expand=True, fill=tk.X, side=tk.TOP)
 
 
 def main():
     init()
-    frame = tk.Frame(root)
-    # padx = padding x, pady = padding y (jarak antar frame)
-    frame.pack(pady=56, padx=180)
+    frame = tk.Frame(root, bg="white", width=400, height=400)
+    frame.pack(pady=56, padx=56)
 
-    tk.Label(frame, text="Name", font=("Open Sans", 12, "bold")).grid(
-        row=0, padx=10, pady=10)
-    userNameField = tk.Entry(frame, font=("Open Sans", 12))
-    # ipadx = padding x, ipady = padding y (jarak antar text)
-    userNameField.grid(row=0, column=1, ipadx=10, ipady=10)
+    tk.Label(frame, text="Name", font=("Open Sans", 12, "bold"), borderwidth=0,
+             highlightthickness=0, bg="white").grid(row=0, padx=10, pady=10)
+    userNameField = tk.Entry(frame, font=(
+        "Open Sans", 12), bd=1, relief="solid")
+    userNameField.grid(row=0, column=1, padx=10, pady=10)
 
     def updateUserInput():
         username = userNameField.get()
@@ -43,18 +45,18 @@ def main():
             game.startGame(root)
             frame.destroy()
 
-    tk.Label(frame, text="Game Level", font=(
-        "Open Sans", 12, "bold")).grid(row=1)
+    tk.Label(frame, text="Game Level", font=("Open Sans", 12, "bold"),
+             borderwidth=0, highlightthickness=0, bg="white").grid(row=1, padx=10, pady=10)
     levelInput = ttk.Combobox(frame, font=("Open Sans", 12), values=[
-        'Easy', 'Medium', 'Hard'])
-    levelInput.grid(row=1, column=1, ipadx=10, ipady=10)
+                              'Easy', 'Medium', 'Hard'])
+    levelInput.grid(row=1, column=1, padx=10, pady=10)
 
-    tk.Button(frame, text="Start Game", font=("Open Sans", 12, "bold"), fg='white', bg='black', command=updateUserInput).grid(  # Command = fungsi yang akan dijalankan ketika tombol ditekan
-        row=2, columnspan=2, ipadx=10, ipady=10)
+    tk.Button(frame, text="Start Game", font=("Open Sans", 12, "bold"), fg='white', bg='green',
+              command=updateUserInput, borderwidth=0, highlightthickness=0).grid(row=2, columnspan=2, padx=10, pady=10)
 
     levelInput.current(0)
 
-    root.mainloop()  # Menjalankan GUI
+    root.mainloop()
 
 
 if __name__ == '__main__':
