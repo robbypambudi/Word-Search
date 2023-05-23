d1691f8f9a30b00470a6cdae96a358f0c67b546f
Robby Pambudi <5025211042@mhs.its.ac.id>
2023-05-23 08:19:08 +0700

    docs: update documentasi

--- src/Game.py
+++ src/Game.py
@@ -13,33 +13,33 @@ route = [0, 0]
 
 
 def startGame(root):
-    config = gutils.readConfigFile()
-    levelNum = config['player']['level']
+    config = gutils.readConfigFile() #  Mengambil data dari file config.yaml
+    levelNum = config['player']['level'] # Mengambil level yang sedang dimainkan
 
     # Timer Frame
-    frame = tk.Frame(root, bg="white", width=400, height=100)
-    frame.pack(fill=tk.BOTH, side=tk.TOP)
-    t.timer(frame, config['levels'][levelNum]['time'])
+    frame = tk.Frame(root, bg="white", width=400, height=100) # Membuat frame untuk timer dan score
+    frame.pack(fill=tk.BOTH, side=tk.TOP) # Mengatur posisi frame
+    t.timer(frame, config['levels'][levelNum]['time']) # Memulai timer
 
     # Vertical Frame
-    frame1 = tk.Frame(master=root, bg="red")
-    frame1.pack(side=tk.LEFT, expand=True, padx=20, pady=20)
+    frame1 = tk.Frame(master=root, bg="red") # Membuat frame untuk grid dan tombol
+    frame1.pack(side=tk.LEFT, expand=True, padx=20, pady=20) 
     frame1.grid_columnconfigure(0, weight=1)
 
     levelDetails = [
         config['levels'][levelNum]['name'],
         config['levels'][levelNum]['words']
-    ]
-    currScore = tk.StringVar()
-    currScore.set(0)
+    ] # Menyimpan data level yang sedang dimainkan
+    currScore = tk.StringVar() # Menyimpan score yang sedang dimainkan
+    currScore.set(0) # Mengatur score awal menjadi 0
 
-    def updateScore(value):
-        config = gutils.readConfigFile()
-        score = config['player']['score']
-        config['player']['score'] = score + value
-        currScore.set(score +value)
-        gutils.writeConfigFile(config)
-        root.update_idletasks()
+    def updateScore(value): # Mengupdate score
+        config = gutils.readConfigFile() # Membaca file config.yaml
+        score = config['player']['score'] # Mengambil score yang sedang dimainkan
+        config['player']['score'] = score + value # Menambahkan score yang sedang dimainkan dengan value
+        currScore.set(score +value) # Mengupdate score yang sedang dimainkan
+        gutils.writeConfigFile(config) # Menulis file config.yaml
+        root.update_idletasks() # Mengupdate tampilan root (GUI)
 
     frame3 = tk.Frame(master=root)
     frame3.pack(fill=tk.BOTH, side=tk.RIGHT, padx=20, pady=30)
@@ -47,40 +47,40 @@ def startGame(root):
     frame4 = tk.Frame(master=root, bg='#cbe5f7', width=400)
     frame4.pack(fill=tk.BOTH, side=tk.TOP, pady=10, anchor='ne', expand=True)
 
-    gutils.labelGame(frame3, config, levelDetails, currScore)
+    gutils.labelGame(frame3, config, levelDetails, currScore) # Membuat label untuk level dan score
 
-    wordList = []
+    wordList = [] # Menyimpan kata-kata yang akan dimainkan
 
-    with open(r'data/words.yaml') as file:
-        wordFile = yaml.load(file, Loader=yaml.FullLoader)
-        wordData = wordFile['words']
-        wordList = [word for word in wordFile['words']]
+    with open(r'data/words.yaml') as file: # Membaca file words.yaml
+        wordFile = yaml.load(file, Loader=yaml.FullLoader) # Mengambil data dari file words.yaml
+        wordData = wordFile['words'] # Menyimpan data dari file words.yaml
+        wordList = [word for word in wordFile['words']] # Menyimpan kata-kata yang akan dimainkan
         
-    curLevel = config['player']['level']
-    numWords= size = config['levels'][curLevel]['words']
+    curLevel = config['player']['level'] # Mengambil level yang sedang dimainkan
+    numWords= size = config['levels'][curLevel]['words'] # Mengambil jumlah kata yang akan dimainkan
         
         
 
-    arr = [[0 for x in range(size)] for y in range(size)]
-    button = [[0 for x in range(size)] for y in range(size)]
-    check = [0 for numWords in range(size)]
-    dictionary = [0 for createWordSet in range(numWords)]
+    arr = [[0 for x in range(size)] for y in range(size)] # Membuat array 2 dimensi dengan ukuran size x size digunakan untuk menyimpan huruf yang akan dimainkan
+    button = [[0 for x in range(size)] for y in range(size)] # Membuat array 2 dimensi dengan ukuran size x size digunakan untuk membuat grid
+    check = [0 for numWords in range(size)] # Membuat array dengan ukuran size x size digunakan untuk mengecek apakah kata sudah ditemukan atau belum
+    dictionary = [0 for createWordSet in range(numWords)] # Membuat array dengan ukuran numWords x numWords digunakan untuk menyimpan kata-kata yang akan dimainkan
 
     directionArr = [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1],
-                    [0, -1], [1, -1]]
+                    [0, -1], [1, -1]]  # Membuat array 2 dimensi dengan ukuran 8 x 2 digunakan untuk menentukan arah kata yang akan dimainkan
 
     def handleButtonHelp():
-        h.help(root, arr, button, dictionary, updateScore)
+        h.help(root, arr, button, dictionary, updateScore) # Memanggil fungsi help dari file help.py
 
     def nextLevel():
-        config = gutils.readConfigFile()
-        config['player']['level'] = config['player']['level'] + 1
-        gutils.writeConfigFile(config)
-        frame.destroy()
-        frame1.destroy()
-        frame4.destroy()
-        frame3.destroy()
-        if (config['player']['level'] == 4):
+        config = gutils.readConfigFile() # Membaca file config.yaml
+        config['player']['level'] = config['player']['level'] + 1 # Menambahkan level yang sedang dimainkan dengan 1
+        gutils.writeConfigFile(config)  # Menulis file config.yaml
+        frame.destroy() # Menghapus frame
+        frame1.destroy() # Menghapus frame
+        frame4.destroy() # Menghapus frame
+        frame3.destroy() # Menghapus frame
+        if (config['player']['level'] == 4): # Jika level yang sedang dimainkan adalah level 4
             msg.showinfo("Selamat!", "Anda telah menyelesaikan semua level!")
             endFrame = tk.Frame(root, bg="white", width=400, height=100)
             endFrame.pack(fill=tk.BOTH, side=tk.TOP)
@@ -117,39 +117,40 @@ def startGame(root):
         filled = False
         char = ''
 
-    def fill(x, y, word, direction):
+    def fill(x, y, word, direction): # Mengisi array arr dengan kata yang akan dimainkan
         w = wordData[word]['WORD']
-        for i in range(len(w)):
-            arr[x + direction[0] * i][y + direction[1] * i].char = w[i]
-            arr[x + direction[0] * i][y + direction[1] * i].filled = True
-        wordList.remove(word)
+        for i in range(len(w)): # Mengisi array arr dengan kata yang akan dimainkan
+            arr[x + direction[0] * i][y + direction[1] * i].char = w[i] # Mengisi array arr dengan kata yang akan dimainkan
+            arr[x + direction[0] * i][y + direction[1] * i].filled = True # Mengisi array arr dengan kata yang akan dimainkan
+        wordList.remove(word) # Menghapus kata yang sudah dimainkan dari array wordList
 
     def wordPlace(j, dictionary):
-        word = random.choice(wordList)
-        direction = directionArr[random.randrange(0, 7)]
-        oneWord = wordData[word]['WORD']
-        x = random.randrange(0, size - 1)
-        y = random.randrange(0, size - 1)
+        word = random.choice(wordList) # Memilih kata secara acak dari array wordList
+        direction = directionArr[random.randrange(0, 7)] # Memilih arah secara acak dari array directionArr
+        oneWord = wordData[word]['WORD'] # Mengambil kata yang akan dimainkan
+        x = random.randrange(0, size - 1) # Memilih posisi x secara acak
+        y = random.randrange(0, size - 1) # Memilih posisi y secara acak
 
-        if (x + len(oneWord) * direction[0] > size - 1
-                or x + len(oneWord) * direction[0] < 0
-                or y + len(oneWord) * direction[1] > size - 1
-            ) or y + len(oneWord) * direction[1] < 0:
-            wordPlace(j, dictionary)
+        if (x + len(oneWord) * direction[0] > size - 1 # Jika kata yang akan dimainkan melebihi ukuran array arr
+                or x + len(oneWord) * direction[0] < 0 # Jika kata yang akan dimainkan melebihi ukuran array arr
+                or y + len(oneWord) * direction[1] > size - 1 # Jika kata yang akan dimainkan melebihi ukuran array arr
+            ) or y + len(oneWord) * direction[1] < 0: # Jika kata yang akan dimainkan melebihi ukuran array arr
+            wordPlace(j, dictionary) # Memanggil fungsi wordPlace
             return
 
-        for i in range(len(oneWord)):
-            if (arr[x + direction[0] * i][y +
-                                          direction[1] * i].filled == True):
-                if (arr[x + direction[0] * i][y + direction[1] * i].char !=
-                        oneWord[i]):
-                    wordPlace(j, dictionary)
+        for i in range(len(oneWord)): # Mengecek apakah kata yang akan dimainkan sudah ada di array arr
+            if (arr[x + direction[0] * i][y + 
+                                          direction[1] * i].filled == True): # Jika kata yang akan dimainkan sudah ada di array arr 
+                if (arr[x + direction[0] * i][y + direction[1] * i].char != 
+                        oneWord[i]): # Lalu jika kata yang akan dimainkan tidak sama dengan kata yang sudah ada di array arr 
+                    wordPlace(j, dictionary) # Memanggil fungsi wordPlace
                     return
+                
         dictionary[j] = {
             'WORD': oneWord,
             'HINT': wordData[word]['HINT'],
             'IS_HELPED': False,
-        }
+        } # Menambahkan kata yang akan dimainkan ke dalam array dictionary
 
         check[j] = tk.Label(frame4,
                             text=wordData[word]['HINT'],
@@ -159,84 +160,84 @@ def startGame(root):
                             fg='#254359',
                             bg='#cbe5f7',
                             anchor='c')
-        check[j].grid(row=j+1, column=0, padx=3, pady=2)
+        check[j].grid(row=j+1, column=0, padx=3, pady=2) # Menampilkan hint dari kata yang akan dimainkan
 
-        fill(x, y, word, direction)
-        return dictionary
+        fill(x, y, word, direction) # Memanggil fungsi fill
+        return dictionary # Mengembalikan array dictionary
 
-    def colourWord(wordPressed, valid):
-        route[0] *= -1
-        route[1] *= -1
-        for i in range(len(wordPressed)):
-            if valid == True or arr[previous[0] +
+    def colourWord(wordPressed, valid): # Mengubah warna dari kata yang sudah dimainkan menjadi biru
+        route[0] *= -1   # Mengubah arah kata yang akan dimainkan
+        route[1] *= -1  # Mengubah arah kata yang akan dimainkan
+        for i in range(len(wordPressed)): # Mengubah warna dari kata yang sudah dimainkan menjadi biru
+            if valid == True or arr[previous[0] + 
                                     i * route[0]][previous[1] +
-                                                  i * route[1]].status == True:
+                                                  i * route[1]].status == True: # Jika kata yang dimainkan benar atau kata yang dimainkan sudah dimainkan sebelumnya
                 button[previous[0] + i * route[0]][previous[1] +
                                                    i * route[1]].config(
                                                        bg='#535edb',
                                                        fg='white')
                 arr[previous[0] + i * route[0]][previous[1] +
-                                                i * route[1]].status = True
+                                                i * route[1]].status = True # Mengubah status dari kata yang dimainkan menjadi True
             elif (arr[previous[0] +
                       i * route[0]][previous[1] +
-                                    i * route[1]].status == False):
+                                    i * route[1]].status == False): # Jika kata yang dimainkan salah dan kata yang dimainkan belum dimainkan sebelumnya
                 button[previous[0] + i * route[0]][previous[1] +
                                                    i * route[1]].config(
                                                        bg='#255059',
-                                                       fg='white')
+                                                       fg='white') # Mengubah warna dari kata yang dimainkan menjadi merah  
 
     def checkWord():
-        global wordPressed
-        dict = [dictionary[i]['WORD']
-                for i in range(len(dictionary)) if dictionary[i] != '']
+        global wordPressed # Mengambil kata yang dimainkan
+        dict = [dictionary[i]['WORD'] 
+                for i in range(len(dictionary)) if dictionary[i] != ''] # Mengambil kata yang akan dimainkan dari array dictionary
 
         if wordPressed in dict and wordPressed != '':
             check[int(dict.index(wordPressed))].configure(
                 font=('Helvetica', 10),  fg='green',
-                bg='#cbe5f7',)
+                bg='#cbe5f7',) # Mengubah warna hint dari kata yang dimainkan menjadi hijau 
 
-            updateScore(10)
+            updateScore(10) # Memanggil fungsi updateScore dengan parameter 10
             colourWord(wordPressed, True)
         else:
-            colourWord(wordPressed, False)
+            colourWord(wordPressed, False) # Memanggil fungsi colourWord dengan parameter False
         wordPressed = ''
         previous = [0, 0]
 
-    def buttonPress(x, y):
-        global wordPressed, previous, route
-        newPressed = [x, y]
+    def buttonPress(x, y): # Mengubah warna dari kata yang dimainkan menjadi biru
+        global wordPressed, previous, route # Mengambil kata yang dimainkan, posisi sebelumnya, dan arah kata yang akan dimainkan
+        newPressed = [x, y] # Mengambil posisi dari kata yang dimainkan
 
-        if (len(wordPressed) == 0):
-            previous = newPressed
-            wordPressed = arr[x][y].char
-            button[x][y].configure(bg='#2c334a', fg='white')
+        if (len(wordPressed) == 0): # Jika kata yang dimainkan belum ada
+            previous = newPressed # Mengubah posisi sebelumnya menjadi posisi kata yang dimainkan
+            wordPressed = arr[x][y].char # Mengambil kata yang dimainkan
+            button[x][y].configure(bg='#2c334a', fg='white') # Mengubah warna dari kata yang dimainkan menjadi biru
 
         elif (len(wordPressed) == 1 and (x - previous[0])**2 <= 1
-              and (y - previous[1])**2 <= 1 and newPressed != previous):
-            wordPressed += arr[x][y].char
-            button[x][y].configure(bg='#2c334a', fg='white')
+              and (y - previous[1])**2 <= 1 and newPressed != previous): # Jika kata yang dimainkan hanya satu dan posisi kata yang dimainkan bersebelahan dengan posisi sebelumnya
+            wordPressed += arr[x][y].char # Mengambil kata yang dimainkan
+            button[x][y].configure(bg='#2c334a', fg='white') # Mengubah warna dari kata yang dimainkan menjadi biru
 
-            route = [x - previous[0], y - previous[1]]
-            previous = [x, y]
+            route = [x - previous[0], y - previous[1]] # Mengubah arah kata yang akan dimainkan
+            previous = [x, y] # Mengubah posisi sebelumnya menjadi posisi kata yang dimainkan
 
         elif (len(wordPressed) > 1 and x - previous[0] == route[0]
-              and y - previous[1] == route[1]):
-            wordPressed += arr[x][y].char
-            button[x][y].configure(bg='#2c334a', fg='white')
-            previous = [x, y]
+              and y - previous[1] == route[1]): # Jika kata yang dimainkan lebih dari satu dan posisi kata yang dimainkan bersebelahan dengan posisi sebelumnya
+            wordPressed += arr[x][y].char # Mengambil kata yang dimainkan
+            button[x][y].configure(bg='#2c334a', fg='white') # Mengubah warna dari kata yang dimainkan menjadi biru
+            previous = [x, y] # Mengubah posisi sebelumnya menjadi posisi kata yang dimainkan
 
-    for x in range(size):
-        for y in range(size):
-            arr[x][y] = Square()
+    for x in range(size): # Membuat grid
+        for y in range(size): # Membuat grid
+            arr[x][y] = Square() # Membuat grid
 
     for i in range(numWords):
-        wordPlace(i, dictionary)
+        wordPlace(i, dictionary) # Memanggil fungsi wordPlace
 
     for y in range(size):
         for x in range(size):
 
             if (arr[x][y].filled == False):
-                arr[x][y].char = random.choice(string.ascii_uppercase)
+                arr[x][y].char = random.choice(string.ascii_uppercase) # Mengisi grid dengan huruf acak
 
             # Button in Grid
             button[x][y] = tk.Button(
@@ -249,7 +250,7 @@ def startGame(root):
                 relief=tk.FLAT,
                 font=('Helvetica', 9),
                 command=lambda x=x, y=y: buttonPress(x, y))
-            button[x][y].grid(row=x, column=y)
+            button[x][y].grid(row=x, column=y) # Membuat grid dengan huruf acak dan memanggil fungsi buttonPress
 
     checkWordBtn = tk.Button(frame4,
                              text="Check Word",
--- src/help.py
+++ src/help.py
@@ -5,64 +5,64 @@ import random
 import src.gameUtils as gutils
 
 def isSafe(arr, x, y, visited):
-    return (x >= 0 and x < len(arr) and y >= 0 and y < len(arr) and not visited[x][y])
+    return (x >= 0 and x < len(arr) and y >= 0 and y < len(arr) and not visited[x][y]) # Cek apakah x dan y berada dalam range array dan belum dikunjungi
 
 
 def checkWordUtil(arr, word, i, j, row, col, visited):
-    loctemp = []
-    xi, yi = i, j
-    temp = 0
-    while (isSafe(arr, xi, yi, visited) and arr[xi][yi].char == word[temp]):
-        loctemp.append([xi, yi])
-        temp += 1
-        xi += row
-        yi += col
-        if temp == len(word):
+    loctemp = [] # Membuat list kosong untuk menampung lokasi kata
+    xi, yi = i, j # Mendefinisikan xi dan yi dengan nilai i dan j
+    temp = 0 # Mendefinisikan temp dengan nilai 0
+    while (isSafe(arr, xi, yi, visited) and arr[xi][yi].char == word[temp]): # Selama x dan y berada dalam range array dan belum dikunjungi dan karakter pada array sama dengan karakter pada kata
+        loctemp.append([xi, yi]) # Menambahkan lokasi x dan y pada list loctemp
+        temp += 1 # Menambahkan nilai temp dengan 1
+        xi += row # Menambahkan nilai xi dengan row
+        yi += col # Menambahkan nilai yi dengan col
+        if temp == len(word): # Jika temp sama dengan panjang kata maka kata ditemukan
             return {'location': loctemp, 'word': word, 'status': True}
 
-    return {'location': [], 'word': [], 'status': False}
+    return {'location': [], 'word': [], 'status': False} # Jika kata tidak ditemukan maka mengembalikan nilai False
 
 
 def checkWord(arr, word, i, j):
-    visited = [[False for i in range(len(arr))] for j in range(len(arr))]
-    row = [-1, -1, -1, 0, 0, 1, 1, 1]
-    col = [-1, 0, 1, -1, 1, -1, 0, 1]
+    visited = [[False for i in range(len(arr))] for j in range(len(arr))] # Membuat array 2 dimensi dengan ukuran array
+    row = [-1, -1, -1, 0, 0, 1, 1, 1] # Mendefinisikan row dengan array 1 dimensi digunakan untuk menentukan arah
+    col = [-1, 0, 1, -1, 1, -1, 0, 1] # Mendefinisikan col dengan array 1 dimensi digunakan untuk menentukan arah
 
-    for direction in range(8):
+    for direction in range(8): # Melakukan perulangan sebanyak 8 kali
         res = checkWordUtil(
-            arr, word, i, j, row[direction], col[direction], visited)
-        if res['status']:
-            return res
-    return {'location': [], 'word': [], 'status': False}
+            arr, word, i, j, row[direction], col[direction], visited) # Memanggil fungsi checkWordUtil dengan parameter array, kata, i, j, row, col, dan visited
+        if res['status']: # Jika status True maka kata ditemukan
+            return res # Mengembalikan nilai res
+    return {'location': [], 'word': [], 'status': False} # Jika kata tidak ditemukan maka mengembalikan nilai False
 
 
 def dfs(arr, word, i, j):
-    visited = [[False for i in range(len(arr))] for j in range(len(arr))]
-    stack = []
-    stack.append([i, j])
-    row = [-1, -1, -1, 0, 0, 1, 1, 1]
-    col = [-1, 0, 1, -1, 1, -1, 0, 1]
+    visited = [[False for i in range(len(arr))] for j in range(len(arr))] # Membuat array 2 dimensi dengan ukuran array
+    stack = [] # Membuat list kosong untuk menampung lokasi kata
+    stack.append([i, j]) # Menambahkan lokasi i dan j pada list stack
+    row = [-1, -1, -1, 0, 0, 1, 1, 1] # Mendefinisikan row dengan array 1 dimensi digunakan untuk menentukan arah
+    col = [-1, 0, 1, -1, 1, -1, 0, 1] # Mendefinisikan col dengan array 1 dimensi digunakan untuk menentukan arah
 
     while stack:
-        x, y = stack.pop()
-        visited[x][y] = True
+        x, y = stack.pop() # Menghapus elemen terakhir pada list stack dan menyimpannya pada variabel x dan y
+        visited[x][y] = True # Mengubah nilai visited pada x dan y menjadi True
         for k in range(8):
-            if isSafe(arr, x + row[k], y + col[k], visited):
-                stack.append([x + row[k], y + col[k]])
-                if arr[x + row[k]][y + col[k]].char == word[0]:
+            if isSafe(arr, x + row[k], y + col[k], visited): # Jika x dan y berada dalam range array dan belum dikunjungi
+                stack.append([x + row[k], y + col[k]]) # Menambahkan lokasi x dan y pada list stack
+                if arr[x + row[k]][y + col[k]].char == word[0]: # Jika karakter pada array sama dengan karakter pada kata
 
-                    res = checkWord(arr, word, x + row[k], y + col[k])
+                    res = checkWord(arr, word, x + row[k], y + col[k]) # Memanggil fungsi checkWord dengan parameter array, kata, x + row[k], dan y + col[k]
                     # Compare res with word
-                    w = res['word']
-                    if (len(w) == len(word)):
-                        for i in range(len(w)):
-                            if w[i] != word[i]:
-                                return {'status': False, 'location': []}
-                        return {'status': True, 'location': res['location']}
+                    w = res['word'] # Mendefinisikan w dengan nilai res['word']
+                    if (len(w) == len(word)): # Jika panjang w sama dengan panjang kata
+                        for i in range(len(w)): # Melakukan perulangan sebanyak panjang w
+                            if w[i] != word[i]: # Jika karakter pada w tidak sama dengan karakter pada kata
+                                return {'status': False, 'location': []} # Mengembalikan nilai False
+                        return {'status': True, 'location': res['location']} # Mengembalikan nilai True dan lokasi kata
 
     return {'status': False, 'location': []}
 
-def checkScore():
+def checkScore(): # Cek apakah score masih lebih dari 0
     config = gutils.readConfigFile()
     score = config['player']['score']
     if (score <= 0):
@@ -75,25 +75,27 @@ def help(root: tk.Tk, arr, button, wordList, updateScore):
     global location
 
     i = 0
-    word = random.choice(wordList)
-    while (True):
+    word = random.choice(wordList) # Memilih kata secara random
+    while (True):   # Memilih kata yang belum dihelp
 
-        if (not word["IS_HELPED"]):
-            word["IS_HELPED"] = True
+        if (not word["IS_HELPED"]): # Jika kata belum dihelp
+            word["IS_HELPED"] = True # Mengubah nilai IS_HELPED menjadi True
             break
         i += 1
-        if (i == len(wordList)):
-            msg.showinfo("Help", "No more words to help")
+        if (i == len(wordList)): # Jika semua kata sudah dihelp
+            msg.showinfo("Help", "No more words to help") # Menampilkan pesan no more words to help
             return
 
-        word = random.choice(wordList)
+        word = random.choice(wordList) # Memilih kata secara random
 
-    res = dfs(arr, word["WORD"], 0, 0)
     #output bantuan saat belum ada nilai
     if (not checkScore()):
-        msg.showinfo("Maaf!", "LOL")
-        return    
+        msg.showinfo("Maaf!", "Jangan Bantuan terus abang LOL")
+        return   
+     
+    res = dfs(arr, word["WORD"], 0, 0) # Memanggil fungsi dfs dengan parameter array, kata, 0, dan 0
     updateScore(-8)
+
     for i in range(len(res['location'])):
         x, y = res['location'][i]
         arr[x][y].filled = True
